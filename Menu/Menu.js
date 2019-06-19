1ecfcdf5d1b9ea8c3ad3e1abba4c4bae789557c6
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-06-19 18:15:17 -0500

    changed my animation codes into component

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -14,59 +14,66 @@ const menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', toggleMenu);
 
 
-// class Animation {
-//   constructor(domElement) {
-//     this.domElement = domElement;
-//     this.clickable = this.domElement.querySelector('li');
+class Animation {
+  constructor(domElement) {
+    this.domElement = domElement;
+    
 
-//     this.clickable.addEventListener('mouseover', this.changeColor.bind(this) );
-//   }
-//   changeColor(){
-//     this.domElement.style.backgroundColor = 'red';
-//   }
-// }
+    this.domElement.addEventListener('mouseover', this.changeColor.bind(this));
+    this.domElement.addEventListener('mouseout', this.backToColor.bind(this));
+  }
+  changeColor(){
+    this.domElement.style.color='red';
+    TweenMax.to('li',1,{scale:1.5,opacity:1})
+  }
+  backToColor(){
+    this.domElement.style.color='black';
+    TweenMax.to('li',1,{scale:1, opacity:0.5,});
+  }
+}
 
 
-// const list = document.querySelectorAll('ul');
-// list.forEach(function(animated){
-//   return new Animation(animated);
+const list = document.querySelectorAll('li');
+list.forEach(function(animated){
+  return new Animation(animated);
+})
+
+// const list = document.querySelectorAll('li')
+
+
+// list[0].addEventListener('mouseover', function(e){
+//   list[0].style.color='red';
+//   TweenMax.to('li',1,{scale:1.5, opacity: 0.5,});
+//   e.preventDefault();
 // })
 
-const list = document.querySelectorAll('li')
+// list[0].addEventListener('mouseout', function(e){
+//   TweenMax.to('li',1,{scale:1, opacity:1,});
+//   list[0].style.color = 'black';
+//   e.preventDefault();
+// })
 
-list[0].addEventListener('mouseover', function(e){
-  list[0].style.color='red';
-  TweenMax.to('li',1,{scale:1.5, opacity: 0.5,});
-  e.preventDefault();
-})
+// list[1].addEventListener('mouseover', function(e){
+//   list[1].style.color='blue';
+//   TweenMax.to('li',1,{scale:1.5});
+//   e.preventDefault();
+// })
 
-list[0].addEventListener('mouseout', function(e){
-  TweenMax.to('li',1,{scale:1, opacity:1,});
-  list[0].style.color = 'black';
-  e.preventDefault();
-})
+// list[1].addEventListener('mouseout', function(e){
+//   TweenMax.to('li',1,{scale:1});
+//   list[1].style.color = 'black';
+//   e.preventDefault();
+// })
 
-list[1].addEventListener('mouseover', function(e){
-  list[1].style.color='blue';
-  TweenMax.to('li',1,{scale:1.5});
-  e.preventDefault();
-})
+// list[2].addEventListener('mouseover', function(e){
+//   list[2].style.color='yellow';
+//   TweenMax.to('li',1,{scale:1.5});
+//   e.preventDefault();
+// })
 
-list[1].addEventListener('mouseout', function(e){
-  TweenMax.to('li',1,{scale:1});
-  list[1].style.color = 'black';
-  e.preventDefault();
-})
-
-list[2].addEventListener('mouseover', function(e){
-  list[2].style.color='yellow';
-  TweenMax.to('li',1,{scale:1.5});
-  e.preventDefault();
-})
-
-list[2].addEventListener('mouseout', function(e){
-  TweenMax.to('li',1,{scale:1});
-  list[2].style.color = 'black';
-  e.preventDefault();
-})
+// list[2].addEventListener('mouseout', function(e){
+//   TweenMax.to('li',1,{scale:1});
+//   list[2].style.color = 'black';
+//   e.preventDefault();
+// })
  
