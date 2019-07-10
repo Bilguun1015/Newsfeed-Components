d7b52597d58c181c3d2024f2a5b68b1ad3031900
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-07-10 17:18:31 -0500

    almost working menubar

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,42 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+const header = document.querySelector('.header');
+menuItems.forEach((list)=>{
+  header.appendChild(menu(list))
+})
+
+function menu(array){
+  const menuBar = document.createElement('div');
+  const list = document.createElement('ul');
+  const listItem1 = document.createElement('li');
+  const listItem2 = document.createElement('li');
+  const listItem3 = document.createElement('li');
+  const listItem4 = document.createElement('li');
+  const listItem5 = document.createElement('li');
+  const listItem6 = document.createElement('li');
+
+  menuBar.appendChild(list);
+  list.appendChild(listItem1);
+  list.appendChild(listItem2);
+  list.appendChild(listItem3);
+  list.appendChild(listItem4);
+  list.appendChild(listItem5);
+  list.appendChild(listItem6);
+
+  menuBar.classList.add('menu');
+
+  listItem1.textContent = array[0];
+  listItem2.textContent = array[1];
+  listItem3.textContent = array[2];
+  listItem4.textContent = array[3];
+  listItem5.textContent = array[4];
+  listItem6.textContent = array[5];
+
+  const buttonMenu = document.querySelector('.menu-button');
+  buttonMenu.addEventListener('click', event=>{
+    menuBar.classList.toggle('menu--open');
+  });
+
+  return menuBar;
+}
